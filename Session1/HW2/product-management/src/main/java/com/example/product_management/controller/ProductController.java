d56f2323ec45e6ef21f00cd72a634243c45d376a
Cung Huynh <ltv.h.cung001@gmail.com>
2026-03-26 08:46:38 +0700

    Done HW4

--- Session1/HW2/product-management/src/main/java/com/example/product_management/controller/ProductController.java
+++ Session1/HW2/product-management/src/main/java/com/example/product_management/controller/ProductController.java
@@ -1,10 +1,20 @@
 package com.example.product_management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.product_management.service.ProductService;
 
+
+@RestController
 public class ProductController {
     @Autowired
     ProductService productService;
+
+    @GetMapping("/api/products")
+    public String getMethodName(@RequestParam String param) {
+        return productService.getAllProducts().toString();
+    }
 }
